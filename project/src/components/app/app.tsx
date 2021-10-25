284889466a18a9f3e697e54ee84fb97b76e17525
Yura <mr.Shapovaloff3101@gmail.com>
2021-10-25 19:55:36 +0300

    2.3. Добавит React-компоненты.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,20 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../main-screen/main-screen';
+
+type MainFilmCard = {
+  title: string;
+  genre: string;
+  year: number;
+};
+
+
+type AppProps = {
+  mainFilmCard: MainFilmCard;
+}
+
+function App({mainFilmCard}: AppProps): JSX.Element {
+  return (
+    <MainScreen mainFilmCard={mainFilmCard} />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const mainFilmCard = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  year: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App mainFilmCard={mainFilmCard} />
   </React.StrictMode>,
   document.getElementById('root'));
