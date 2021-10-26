9c73ba990a6b112fb6c2c926ff0cb5e10d5d4907
Yura <mr.Shapovaloff3101@gmail.com>
2021-10-26 20:57:40 +0300

    3.2. Добавит компонент Logo.

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SmallFilmCard from '../small-film-card/small-film-card';
+import Logo from '../logo/logo';
 
 type MainFilmCard = {
   title: string;
@@ -26,13 +27,7 @@ function MainScreen({mainFilmCard}: MainScreenProps): JSX.Element {
         <h1 className="visually-hidden">WTW</h1>
 
         <header className="page-header film-card__head">
-          <div className="logo">
-            <a className="logo__link">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo />
 
           <ul className="user-block">
             <li className="user-block__item">
@@ -127,13 +122,7 @@ function MainScreen({mainFilmCard}: MainScreenProps): JSX.Element {
         </section>
 
         <footer className="page-footer">
-          <div className="logo">
-            <a className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </a>
-          </div>
+          <Logo team="light" />
 
           <div className="copyright">
             <p>© 2019 What to watch Ltd.</p>
