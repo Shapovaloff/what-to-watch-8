08126722724ba1b23a6e99d2eaa9262949b474f0
Yura <mr.Shapovaloff3101@gmail.com>
2021-10-26 20:28:42 +0300

    3.1. Добавит маршрутизацию.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
 import MainScreen from '../main-screen/main-screen';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import MyListscreen from '../my-list-screen/my-list-screen';
+import LoginScreen from '../login-screen/login-screen';
+import FilmScreen from '../film-screen/film-screen';
+import ReviewScreen from '../review-screen/review-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 type MainFilmCard = {
   title: string;
@@ -13,7 +20,29 @@ type AppProps = {
 
 function App({mainFilmCard}: AppProps): JSX.Element {
   return (
-    <MainScreen mainFilmCard={mainFilmCard} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Root}>
+          <MainScreen mainFilmCard={mainFilmCard} />
+        </Route>
+        <Route exact path={AppRoute.MyList}>
+          <MyListscreen />
+        </Route>
+        <Route exact path={AppRoute.Login}>
+          <LoginScreen />
+        </Route>
+        <Route exact path={AppRoute.Film}>
+          <FilmScreen />
+        </Route>
+        <Route exact path={AppRoute.Review}>
+          <ReviewScreen />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
---
+++ project/src/const.ts
@@ -0,0 +1,7 @@
+export enum AppRoute {
+  Login = '/login',
+  Root = '/',
+  MyList = '/my-list',
+  Film = '/film/:id',
+  Review = '/player/:id'
+}
