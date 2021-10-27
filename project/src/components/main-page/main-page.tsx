3cabf4fd43fc6cc49bac80707d38a23b76eff126
Yura <mr.Shapovaloff3101@gmail.com>
2021-10-27 17:49:07 +0300

    4.6. Создаст новый компонеты FilmCardMain и CatalogMoreButton.

--- project/src/components/main-page/main-page.tsx
+++ project/src/components/main-page/main-page.tsx
@@ -3,6 +3,8 @@ import Logo from '../logo/logo';
 import {Film} from '../../types/film';
 import CatalogFilmsList from '../catalog-films-list/catalog-films-list';
 import CatalogGenresList from '../catalog-genres-list/catalog-genres-list';
+import FilmCardMain from '../film-card-main/film-card-main';
+import CatalogMoreButton from '../catalog-more-button/catalog-more-button';
 
 type MainPageProps = {
   films: Film[];
@@ -13,59 +15,7 @@ function MainPage({films}: MainPageProps): JSX.Element {
 
   return (
     <>
-      <section className="film-card">
-        <div className="film-card__bg">
-          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
-        </div>
-
-        <h1 className="visually-hidden">WTW</h1>
-
-        <header className="page-header film-card__head">
-          <Logo />
-
-          <ul className="user-block">
-            <li className="user-block__item">
-              <div className="user-block__avatar">
-                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63"/>
-              </div>
-            </li>
-            <li className="user-block__item">
-              <a className="user-block__link">Sign out</a>
-            </li>
-          </ul>
-        </header>
-
-        <div className="film-card__wrap">
-          <div className="film-card__info">
-            <div className="film-card__poster">
-              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327"/>
-            </div>
-
-            <div className="film-card__desc">
-              <h2 className="film-card__title">{promoFilm.name}</h2>
-              <p className="film-card__meta">
-                <span className="film-card__genre">{promoFilm.genre}</span>
-                <span className="film-card__year">{promoFilm.released}</span>
-              </p>
-
-              <div className="film-card__buttons">
-                <button className="btn btn--play film-card__button" type="button">
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button className="btn btn--list film-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <FilmCardMain film={promoFilm} />
 
       <div className="page-content">
         <section className="catalog">
@@ -73,10 +23,7 @@ function MainPage({films}: MainPageProps): JSX.Element {
 
           <CatalogGenresList />
           <CatalogFilmsList films={films} />
-
-          <div className="catalog__more">
-            <button className="catalog__button" type="button">Show more</button>
-          </div>
+          <CatalogMoreButton />
         </section>
 
         <footer className="page-footer">
