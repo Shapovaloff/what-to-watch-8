0a2497531c7dbe3156bc00eb43eb3aa41da74054
Yura <mr.Shapovaloff3101@gmail.com>
2021-10-27 11:51:43 +0300

    4.1. Добавит моки фильмов.

---
+++ project/src/moks/films.ts
@@ -0,0 +1,183 @@
+import {Film} from '../types/films';
+
+export const films: Film[] = [
+  {
+    name: "Macbeth",
+    posterImage: "https://8.react.pages.academy/static/film/poster/Macbeth.jpg",
+    previewImage: "https://8.react.pages.academy/static/film/preview/macbeth.jpg",
+    backgroundImage: "https://8.react.pages.academy/static/film/background/Macbeth.jpg",
+    backgroundColor: "#F1E9CE",
+    description: "Macbeth, the Thane of Glamis, receives a prophecy from a trio of witches that one day he will become King of Scotland. Consumed by ambition and spurred to action by his wife, Macbeth murders his king and takes the throne for himself.",
+    rating: 3.3,
+    scoresCount: 48798,
+    director: "Justin Kurzel",
+    starring: [
+      "Michael Fassbender",
+      "Marion Cotillard",
+      "Jack Madigan"
+    ],
+    runTime: 113,
+    genre: "Drama",
+    released: 2015,
+    id: 1,
+    isFavorite: true,
+    videoLink: "http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
+    previewVideoLink: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+  }, {
+    name: "A Star Is Born",
+    posterImage: "https://8.react.pages.academy/static/film/poster/A_Star_Is_Born.jpg",
+    previewImage: "https://8.react.pages.academy/static/film/preview/A_Star_Is_Born.jpg",
+    backgroundImage: "https://8.react.pages.academy/static/film/background/A_Star_is_Born.jpg",
+    backgroundColor: "#C4C0C0",
+    description: "A musician helps a young singer find fame as age and alcoholism send his own career into a downward spiral.",
+    rating: 3.9,
+    scoresCount: 244484,
+    director: "Bradley Cooper",
+    starring: [
+      "Lady Gaga",
+      "Bradley Cooper",
+      "Sam Elliott"
+    ],
+    runTime: 136,
+    genre: "Drama",
+    released: 2018,
+    id: 2,
+    isFavorite: true,
+    videoLink: "http://media.xiph.org/mango/tears_of_steel_1080p.webm",
+    previewVideoLink: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+  }, {
+    name: "No Country for Old Men",
+    posterImage: "https://8.react.pages.academy/static/film/poster/No_Country_for_Old_Men.jpg",
+    previewImage: "https://8.react.pages.academy/static/film/preview/no-country-for-old-men.jpg",
+    backgroundImage: "https://8.react.pages.academy/static/film/background/No_Country_for_Old_Men.jpg",
+    backgroundColor: "#BDAD8F",
+    description: "Violence and mayhem ensue after a hunter stumbles upon a drug deal gone wrong and more than two million dollars in cash near the Rio Grande.",
+    rating: 4.1,
+    scoresCount: 764976,
+    director: "Ethan Coen",
+    starring: [
+      "Tommy Lee Jones",
+      "Javier Bardem",
+      "Josh Brolin"
+    ],
+    runTime: 122,
+    genre: "Crime",
+    released: 2007,
+    id: 3,
+    isFavorite: true,
+    videoLink: "http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
+    previewVideoLink: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+  }, {
+    name: "Pulp Fiction",
+    posterImage: "https://8.react.pages.academy/static/film/poster/Pulp_Fiction.jpg",
+    previewImage: "https://8.react.pages.academy/static/film/preview/pulp-fiction.jpg",
+    backgroundImage: "https://8.react.pages.academy/static/film/background/Pulp_Fiction.jpg",
+    backgroundColor: "#795433",
+    description: "The lives of two mob hitmen, a boxer, a gangster & his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    rating: 1.5,
+    scoresCount: 1635992,
+    director: "Quentin Tarantino",
+    starring: [
+      "John Travolta",
+      "Uma Thurman",
+      "Samuel L. Jackson"
+    ],
+    runTime: 153,
+    genre: "Crime",
+    released: 1994,
+    id: 4,
+    isFavorite: false,
+    videoLink: "http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
+    previewVideoLink: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+  }, {
+    name: "We need to talk about Kevin",
+    posterImage: "https://8.react.pages.academy/static/film/poster/We_need_to_talk_about_Kevin.jpg",
+    previewImage: "https://8.react.pages.academy/static/film/preview/we-need-to-talk-about-kevin.jpg",
+    backgroundImage: "https://8.react.pages.academy/static/film/background/We_need_to_talk_about_Kevin.jpg",
+    backgroundColor: "#E1DFDE",
+    description: "Kevin's mother struggles to love her strange child, despite the increasingly dangerous things he says and does as he grows up. But Kevin is just getting started, and his final act will be beyond anything anyone imagined.",
+    rating: 3.8,
+    scoresCount: 123240,
+    director: "Lynne Ramsay",
+    starring: [
+      "Tilda Swinton",
+      "John C. Reilly",
+      "Ezra Miller"
+    ],
+    runTime: 112,
+    genre: "Drama",
+    released: 2011,
+    id: 5,
+    isFavorite: true,
+    videoLink: "http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
+    previewVideoLink: "https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm"
+  }, {
+    name: "Seven Years in Tibet",
+    posterImage: "https://8.react.pages.academy/static/film/poster/Seven_Years_in_Tibet.jpg",
+    previewImage: "https://8.react.pages.academy/static/film/preview/seven-years-in-tibet.jpg",
+    backgroundImage: "https://8.react.pages.academy/static/film/background/Seven_Years_in_Tibet.jpg",
+    backgroundColor: "#C6CADF",
+    description: "True story of Heinrich Harrer, an Austrian mountain climber who became friends with the Dalai Lama at the time of China's takeover of Tibet.",
+    rating: 3.6,
+    scoresCount: 112612,
+    director: "Jean-Jacques Annaud",
+    starring: [
+      "Brad Pitt",
+      "David Thewlis",
+      "BD Wong"
+    ],
+    runTime: 136,
+    genre: "Adventure",
+    released: 1997,
+    id: 6,
+    isFavorite: false,
+    videoLink: "http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
+    previewVideoLink: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+  }, {
+    name: "Beach",
+    posterImage: "https://8.react.pages.academy/static/film/poster/beach.jpg",
+    previewImage: "https://8.react.pages.academy/static/film/preview/beach.jpg",
+    backgroundImage: "https://8.react.pages.academy/static/film/background/beach.jpg",
+    backgroundColor: "#EBC996",
+    description: "Vicenarian Richard travels to Thailand and finds himself in possession of a strange map. Rumours state that it leads to a solitary beach paradise, a tropical bliss. Excited and intrigued, he sets out to find it.",
+    rating: 3.3,
+    scoresCount: 207824,
+    director: "Danny Boyle",
+    starring: [
+      "Leonardo DiCaprio",
+      "Daniel York",
+      "Patcharawan Patarakijjanon"
+    ],
+    runTime: 119,
+    genre: "Adventure",
+    released: 2000,
+    id: 7,
+    isFavorite: false,
+    videoLink: "http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
+    previewVideoLink: "https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4"
+  }, {
+    name: "Snatch",
+    posterImage: "https://8.react.pages.academy/static/film/poster/Snatch.jpg",
+    previewImage: "https://8.react.pages.academy/static/film/preview/snatch.jpg",
+    backgroundImage: "https://8.react.pages.academy/static/film/background/Snatch.jpg",
+    backgroundColor: "#FDFDFC",
+    description: "Unscrupulous boxing promoters, violent bookmakers, a Russian gangster, incompetent amateur robbers and supposedly Jewish jewelers fight to track down a priceless stolen diamond.",
+    rating: 0.2,
+    scoresCount: 716577,
+    director: "Guy Ritchie",
+    starring: [
+      "Jason Statham",
+      "Brad Pitt",
+      "Benicio Del Toro"
+    ],
+    runTime: 104,
+    genre: "Comedy",
+    released: 2000,
+    id: 8,
+    isFavorite: true,
+    videoLink: "http://peach.themazzone.com/durian/movies/sintel-1024-surround.mp4",
+    previewVideoLink: "https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm"
+  },
+];
+
+
