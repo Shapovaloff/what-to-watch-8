95693b8f4f109c4cc9aee501765540b5391ca656
Yura <mr.Shapovaloff3101@gmail.com>
2021-11-08 19:35:20 +0300

    6.2. Подключит redux к проекту.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 import {films} from './moks/films';
 import {comments} from './moks/comments';
+import {Provider} from 'react-redux';
+import {createStore} from '@reduxjs/toolkit';
+import {reducer} from './store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App films={films} comments={comments} />
+    <Provider store={store}>
+      <App films={films} comments={comments} />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
